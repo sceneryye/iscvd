6fdad10e28a140123f0ca895f71cbd1f1c41c86c
Scenery Ye <scenery.ye@gmail.com>
2016-03-30 17:18:01 +0800

    change de

--- db/migrate/20160330020803_create_meetings.rb
+++ db/migrate/20160330020803_create_meetings.rb
@@ -1,9 +1,11 @@
 class CreateMeetings < ActiveRecord::Migration
   def change
   	create_table :meetings,:options=>"ENGINE=MyISAM DEFAULT CHARSET=utf8" do |t|
-        t.string :title
+        t.string :title_en
+        t.string :location_en
+        t.string :title_zh
+        t.string :location_zh
         t.string :slug, :unique=>true,:null=>false
-        t.string :location
         t.datetime :start_time
         t.datetime :end_time
 
