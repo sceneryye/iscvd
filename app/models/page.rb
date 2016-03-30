7fcbd0ec5c1688f627eb6caf4ab00fd11a95997f
Scenery Ye <scenery.ye@gmail.com>
2016-03-30 18:10:33 +0800

    front pages

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,11 +1,15 @@
 class Page < ActiveRecord::Base
-  belongs_to :meeting
-  belongs_to :page_category
-  has_many :comments, dependent: :destroy
+	extend FriendlyId
+	
+	friendly_id :url, :use => [:slugged, :finders]
+	
+	belongs_to :meeting
+	belongs_to :page_category
+	has_many :comments, dependent: :destroy
 
-  validates :title, presence: true
-  validates :body,  presence: true, length: {minimum: 2}
-  validates :language, presence: true
+	validates :title, presence: true
+	validates :body,  presence: true, length: {minimum: 2}
+	validates :language, presence: true
 
-  default_scope {order 'created_at DESC'}
+	default_scope {order 'created_at DESC'}
 end
