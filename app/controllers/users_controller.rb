abeec1b015108630879c17cf202ad142e9ebf626
Scenery Ye <scenery.ye@gmail.com>
2016-05-09 21:21:25 +0800

    institute & hospital

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -95,6 +95,6 @@ class UsersController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_digest, :full_name, :role, :avatar, :mobile, :sex,
-                                 :password_confirmation,:area,:address,:insitute_hospital)
+                                 :password_confirmation,:area,:address,:institute_hospital)
   end
 end
